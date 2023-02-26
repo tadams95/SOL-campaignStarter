a8e591b4ada3b19c5cb30a3849fbbaa96f160f36
tadams95 <tyrelleadams28@gmail.com>
2023-02-26 02:10:44 -0800

    Refactor truffle-config and contract deployment

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,11 +1,10 @@
 const CampaignFactory = artifacts.require("CampaignFactory");
 const Campaign = artifacts.require("Campaign");
 
-module.exports = async function(deployer) {
+module.exports = async function (deployer) {
   // Use deployer to state migration tasks.
   await deployer.deploy(CampaignFactory);
-const campaignFactory = await CampaignFactory.deployed();
-console.log(campaignFactory.devdoc);
-minimum = 1000000
-  await deployer.deploy(Campaign, minimum, campaignFactory.address );
+  const campaignFactory = await CampaignFactory.deployed();
+  minimum = 100;
+  await deployer.deploy(Campaign, minimum, campaignFactory.address);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -56,7 +56,7 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
-
+  contracts_build_directory: "../client/src/contracts",
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
@@ -106,7 +106,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.17",      // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.17", // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
@@ -115,7 +115,7 @@ module.exports = {
       //  },
       //  evmVersion: "byzantium"
       // }
-    }
+    },
   },
 
   // Truffle DB is currently disabled by default; to enable it, change enabled:
