1e19390a7852521fe061a163822f2404378b0862
tadams95 <tyrelleadams28@gmail.com>
2023-02-27 11:59:11 -0800

    Form Header and import into Layout

---
+++ client2/components/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+
+
+// handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+const Header = (props) => {
+  return (
+    <div>
+      <Menu>
+        <Menu.Item
+          name="CrowdStarter"
+          //   active={activeItem === "browse"}
+          //   onClick={this.handleItemClick}
+        />
+         <Menu.Menu position='right'>
+          <Menu.Item
+            name='Campaigns'
+            // active={activeItem === 'signup'}
+            // onClick={this.handleItemClick}
+          >
+            Campaigns
+          </Menu.Item>
+
+          <Menu.Item
+            name='add'
+            // active={activeItem === 'help'}
+            // onClick={this.handleItemClick}
+          >
+            +
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+      {props.children}
+    </div>
+  );
+};
+
+export default Header;
