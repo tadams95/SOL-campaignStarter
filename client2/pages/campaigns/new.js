a4ff747eac005dd30d2cd37c73052776b51ca91a
tadams95 <tyrelleadams28@gmail.com>
2023-02-27 13:31:28 -0800

    Wire form submittal to smart contract functions

--- client2/pages/campaigns/new.js
+++ client2/pages/campaigns/new.js
@@ -1,16 +1,40 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
-import { Form, Button } from "semantic-ui-react";
+import { Form, Button, Input } from "semantic-ui-react";
+import factory from "../../web3/factory";
+import web3 from "../../web3/web3";
 
 class CampaignNew extends Component {
+  state = {
+    minimumContribution: "",
+  };
+
+  //form submittal handler
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+    //create a new campaign
+    await factory.methods.createCampaign(this.state.minimumContribution).send({
+      from: accounts[0],
+    });
+  };
+
   render() {
     return (
       <Layout>
         <h3>Create a campaign</h3>
-        <Form>
+        <Form onSubmit={this.onSubmit}>
           <Form.Field>
             <label>Minimum Contribution</label>
-            <input />
+            <Input
+              label="Wei"
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={(event) =>
+                this.setState({ minimumContribution: event.target.value })
+              }
+            />
           </Form.Field>
           <Button primary>Create!</Button>
         </Form>
