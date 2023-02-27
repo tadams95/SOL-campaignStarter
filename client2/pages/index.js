8db7c0de183dcbc44a7f98b62b61fcff0ec4b7aa
tadams95 <tyrelleadams28@gmail.com>
2023-02-26 16:10:54 -0800

    Render Campaign Card

--- client2/pages/index.js
+++ client2/pages/index.js
@@ -1,22 +1,32 @@
 import React, { Component } from "react";
 import factory from "../web3/factory";
-import styles from "@/styles/Home.module.css";
-import { Button } from "semantic-ui-react";
+// import styles from "@/styles/Home.module.css";
+import { Button, Card } from "semantic-ui-react";
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
-   
+
     return { campaigns };
+  }
+
+  renderCampaigns() {
+    const items = this.props.campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true,
+      };
+    });
+
+    return <Card.Group items={items} />;
   }
 
   render() {
     return (
       <>
-        <div className={styles.description}>{this.props.campaigns[0]}</div>
-        
+        <div>{this.renderCampaigns()}</div>
       </>
-      
     );
     //test
   }
