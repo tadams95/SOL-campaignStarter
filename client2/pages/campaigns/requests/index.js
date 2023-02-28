66e01ac589daca5c6f78833a75dac50ca2ba41e2
tadams95 <tyrelleadams28@gmail.com>
2023-02-28 14:10:03 -0800

    Render Request Table

--- client2/pages/campaigns/requests/index.js
+++ client2/pages/campaigns/requests/index.js
@@ -1,24 +1,55 @@
 import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Table } from "semantic-ui-react";
 import { Link } from "../../../routes";
 import Layout from "../../../components/Layout";
+import Campaign from "../../../web3/campaign";
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
     const { address } = props.query;
+    const campaign = Campaign(address);
 
-    return { address };
+    const requestCount = await campaign.methods.getRequestsCount().call();
+
+    const requests = await Promise.all(
+      Array(parseInt(requestCount))
+        .fill()
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
+        })
+    );
+    return { address, requests, requestCount };
   }
 
   render() {
+    const { Header, Row, HeaderCell, Body } = Table;
+
     return (
       <Layout>
         <h3>Requests</h3>
-        <Link legacyBehavior route={`/campaigns/${this.props.address}/requests/new`}>
+        <Link
+          legacyBehavior
+          route={`/campaigns/${this.props.address}/requests/new`}
+        >
           <a>
             <Button primary>Add Request</Button>
           </a>
         </Link>
+    
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+          {/* <Body>{this.renderRows()}</Body> */}
+        </Table>
       </Layout>
     );
   }
